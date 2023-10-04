c3ba4e2d32434b4e237367d16411dcda83677579
mohammedBamatraf <mohammed.ab@bootfi.com>
2023-10-04 15:17:30 +0300

    create seed and factory

---
+++ database/seeders/CollectionSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Collection;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CollectionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users=User::select('id')->get();
+        for($i=0;$i<=10;$i++)
+        {
+            Collection::factory()->create(['user_id' => $users->random()->id]);
+        }
+    }
+}
---
+++ database/seeders/GroupSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Collection;
+use App\Models\Group;
+use App\Models\Language;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GroupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = Category::select('id')->get();
+        $collections = Collection::select('id')->get();
+        $languages = Language::select('id')->get();
+
+        for ($i = 0; $i <= 100; $i++) {
+            Group::factory()->create([
+                'category_id' => $categories->random()->id,
+                'collection_id' => $collections->random()->id,
+                'language_id'   => $languages->random()->id,
+            ]);
+        }
+    }
+}
