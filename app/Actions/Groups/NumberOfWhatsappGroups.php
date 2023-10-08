5700ecd78e92e0b26a63de9c1e2fb58649daa5ee
mohammedBamatraf <mohammed.ab@bootfi.com>
2023-10-08 18:37:18 +0300

    update number of telegram and  whatsapp groups

--- app/Actions/Groups/NumberOfWhatsappGroups.php
+++ app/Actions/Groups/NumberOfWhatsappGroups.php
@@ -8,7 +8,7 @@ class NumberOfWhatsappGroups
 
     public function __invoke()
     {
-       $num_of_groups = Group::where([['social_type',0],['is_active',1]])->count();
+       $num_of_groups = Group::where([['social_type','whatsapp'],['is_active',1]])->count();
         return $num_of_groups;
     }
 
