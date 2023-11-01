0889421a37ca0ee7d765b6cbe4d89939cc0750c3
mohammedBamatraf <mohammed.ab@bootfi.com>
2023-11-01 16:02:54 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::prefix('admin')->group(function () {
 
 Route::prefix('user')->group(function () {
 
-    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
     Route::delete('/delete-account', [AuthController::class, 'deleteAccount'])->middleware('auth:api');
